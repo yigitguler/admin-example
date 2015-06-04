73518861df33811d675457eede81ff8f29ff7a6c
Yigit Guler <yigit@hipo.biz>
2015-06-04 12:01:13 +0100

    Added new files

--- cardiffshop/products/admin.py
+++ cardiffshop/products/admin.py
@@ -1,7 +1,42 @@
 from products.models import Product, Category, ProductImage
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
+
+class CanBeSoldListFilter(SimpleListFilter):
+    title = "Can be sold"
+    parameter_name = "can_be_sold"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("1", "Yes"),
+            ("0", "No")
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "1":
+            return queryset.filter(stock_count__gt=0)
+        if value == "0":
+            return queryset.filter(stock_count=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "stock_count", "can_be_sold")
+    list_filter = ("category", "date_created", CanBeSoldListFilter)
+    search_fields = ("name", "description", "sku_number", "barcode")
+
+    def can_be_sold(self, obj):
+        """
+        Determines whether the product can be sold or not.
+        """
+        if obj.stock_count > 0:
+            return True
+        else:
+            return False
+
+    can_be_sold.boolean = True
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImage)
